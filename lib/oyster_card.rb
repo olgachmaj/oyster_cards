d47d65e7d1745178c141cb6133c4e8de10e68aae
Max Clarke <maxclarke@MA368.local>
2021-02-08 14:57:08 +0000

    added oystercard class, and balance method

---
+++ lib/oyster_card.rb
@@ -0,0 +1,6 @@
+class OysterCard
+  attr_reader :balance
+  def initialize(balance = 0)
+    @balance = balance
+  end
+end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,4 +1,11 @@
+require 'oyster_card'
+
 describe OysterCard do
+  #checking if oystercard has the method of balance
+  it { is_expected.to respond_to :balance }
 
-
+  #checking that balance returns the balance
+  it 'has default balance of 0 when new' do
+  expect(subject.balance).to eq 0
+  end
 end
