4b9822da0162814fe9e85b44610065c991bb26f9
Max Clarke <maxclarke@MA368.local>
2021-02-08 15:17:01 +0000

    added topup method and updated tests

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,4 +3,7 @@ class OysterCard
   def initialize(balance = 0)
     @balance = balance
   end
+  def top_up(amount)
+    @balance += amount
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -8,4 +8,14 @@ describe OysterCard do
   it 'has default balance of 0 when new' do
   expect(subject.balance).to eq 0
   end
+
+  #checking if oytercard has the method topup
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  #we want to topup the card with an amount and see that the balance of the card has been updated with amount
+  it 'top_up oystercard with amount and check balance' do
+    current_balance = subject.balance
+    subject.top_up(amount = 5)
+    expect(subject.balance).to eq current_balance + amount
+  end
 end
